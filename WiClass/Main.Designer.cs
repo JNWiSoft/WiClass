d725daadf2fc1c6bae790e840fe7d4d04dd8ea12
Luis <1978L@163.com>
2017-11-06 17:04:40 +0800

    界面实现

---
+++ WiClass/Main.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WiClass
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlToolsbar = new System.Windows.Forms.Panel();
+            this.picStartMenuico = new System.Windows.Forms.PictureBox();
+            this.picBackground = new System.Windows.Forms.PictureBox();
+            this.ucMenuItem1 = new WiClass.UControl.ucMenuItem();
+            this.ucMini = new WiClass.UControl.ucMenuItem();
+            this.ucRedo = new WiClass.UControl.ucMenuItem();
+            this.ucBack = new WiClass.UControl.ucMenuItem();
+            this.ucBackground = new WiClass.UControl.ucMenuItem();
+            this.ucTools = new WiClass.UControl.ucMenuItem();
+            this.ucSelect = new WiClass.UControl.ucMenuItem();
+            this.ucEraser = new WiClass.UControl.ucMenuItem();
+            this.ucPen = new WiClass.UControl.ucMenuItem();
+            this.pnlToolsbar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picStartMenuico)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBackground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlToolsbar
+            // 
+            this.pnlToolsbar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlToolsbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.pnlToolsbar.Controls.Add(this.ucMenuItem1);
+            this.pnlToolsbar.Controls.Add(this.ucMini);
+            this.pnlToolsbar.Controls.Add(this.ucRedo);
+            this.pnlToolsbar.Controls.Add(this.ucBack);
+            this.pnlToolsbar.Controls.Add(this.ucBackground);
+            this.pnlToolsbar.Controls.Add(this.ucTools);
+            this.pnlToolsbar.Controls.Add(this.ucSelect);
+            this.pnlToolsbar.Controls.Add(this.ucEraser);
+            this.pnlToolsbar.Controls.Add(this.ucPen);
+            this.pnlToolsbar.Controls.Add(this.picStartMenuico);
+            this.pnlToolsbar.Location = new System.Drawing.Point(0, 556);
+            this.pnlToolsbar.Name = "pnlToolsbar";
+            this.pnlToolsbar.Size = new System.Drawing.Size(1336, 66);
+            this.pnlToolsbar.TabIndex = 0;
+            // 
+            // picStartMenuico
+            // 
+            this.picStartMenuico.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.picStartMenuico.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picStartMenuico.Image = global::WiClass.Properties.Resources.logo_50_;
+            this.picStartMenuico.Location = new System.Drawing.Point(0, 0);
+            this.picStartMenuico.Name = "picStartMenuico";
+            this.picStartMenuico.Padding = new System.Windows.Forms.Padding(5);
+            this.picStartMenuico.Size = new System.Drawing.Size(66, 66);
+            this.picStartMenuico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picStartMenuico.TabIndex = 0;
+            this.picStartMenuico.TabStop = false;
+            this.picStartMenuico.MouseEnter += new System.EventHandler(this.picStartMenuico_MouseEnter);
+            this.picStartMenuico.MouseLeave += new System.EventHandler(this.picStartMenuico_MouseLeave);
+            this.picStartMenuico.MouseHover += new System.EventHandler(this.picStartMenuico_MouseHover);
+            // 
+            // picBackground
+            // 
+            this.picBackground.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBackground.Location = new System.Drawing.Point(0, 0);
+            this.picBackground.Margin = new System.Windows.Forms.Padding(0);
+            this.picBackground.Name = "picBackground";
+            this.picBackground.Size = new System.Drawing.Size(1336, 557);
+            this.picBackground.TabIndex = 1;
+            this.picBackground.TabStop = false;
+            // 
+            // ucMenuItem1
+            // 
+            this.ucMenuItem1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucMenuItem1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(102)))), ((int)(((byte)(255)))));
+            this.ucMenuItem1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ucMenuItem1.Imagesurl = global::WiClass.Properties.Resources.interaction;
+            this.ucMenuItem1.ItemText = "互动";
+            this.ucMenuItem1.Location = new System.Drawing.Point(1269, 0);
+            this.ucMenuItem1.Margin = new System.Windows.Forms.Padding(0);
+            this.ucMenuItem1.Name = "ucMenuItem1";
+            this.ucMenuItem1.Size = new System.Drawing.Size(66, 66);
+            this.ucMenuItem1.TabIndex = 8;
+            // 
+            // ucMini
+            // 
+            this.ucMini.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucMini.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ucMini.Imagesurl = global::WiClass.Properties.Resources.mini;
+            this.ucMini.ItemText = "桌面";
+            this.ucMini.Location = new System.Drawing.Point(895, 0);
+            this.ucMini.Name = "ucMini";
+            this.ucMini.Size = new System.Drawing.Size(66, 66);
+            this.ucMini.TabIndex = 7;
+            // 
+            // ucRedo
+            // 
+            this.ucRedo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucRedo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ucRedo.Imagesurl = global::WiClass.Properties.Resources.redo;
+            this.ucRedo.ItemText = "恢复";
+            this.ucRedo.Location = new System.Drawing.Point(827, 0);
+            this.ucRedo.Name = "ucRedo";
+            this.ucRedo.Size = new System.Drawing.Size(66, 66);
+            this.ucRedo.TabIndex = 6;
+            // 
+            // ucBack
+            // 
+            this.ucBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ucBack.Imagesurl = global::WiClass.Properties.Resources.back;
+            this.ucBack.ItemText = "撤销";
+            this.ucBack.Location = new System.Drawing.Point(759, 0);
+            this.ucBack.Name = "ucBack";
+            this.ucBack.Size = new System.Drawing.Size(66, 66);
+            this.ucBack.TabIndex = 5;
+            // 
+            // ucBackground
+            // 
+            this.ucBackground.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucBackground.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ucBackground.Imagesurl = global::WiClass.Properties.Resources.background;
+            this.ucBackground.ItemText = "背景";
+            this.ucBackground.Location = new System.Drawing.Point(691, 0);
+            this.ucBackground.Name = "ucBackground";
+            this.ucBackground.Size = new System.Drawing.Size(66, 66);
+            this.ucBackground.TabIndex = 4;
+            // 
+            // ucTools
+            // 
+            this.ucTools.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucTools.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ucTools.Imagesurl = global::WiClass.Properties.Resources.tools;
+            this.ucTools.ItemText = "工具";
+            this.ucTools.Location = new System.Drawing.Point(623, 0);
+            this.ucTools.Name = "ucTools";
+            this.ucTools.Size = new System.Drawing.Size(66, 66);
+            this.ucTools.TabIndex = 3;
+            // 
+            // ucSelect
+            // 
+            this.ucSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucSelect.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ucSelect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ucSelect.Imagesurl = global::WiClass.Properties.Resources.select;
+            this.ucSelect.ItemText = "选择";
+            this.ucSelect.Location = new System.Drawing.Point(419, 0);
+            this.ucSelect.Name = "ucSelect";
+            this.ucSelect.Size = new System.Drawing.Size(66, 66);
+            this.ucSelect.TabIndex = 1;
+            // 
+            // ucEraser
+            // 
+            this.ucEraser.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucEraser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ucEraser.Imagesurl = global::WiClass.Properties.Resources.eraser;
+            this.ucEraser.ItemText = "橡皮";
+            this.ucEraser.Location = new System.Drawing.Point(555, 0);
+            this.ucEraser.Name = "ucEraser";
+            this.ucEraser.Size = new System.Drawing.Size(66, 66);
+            this.ucEraser.TabIndex = 2;
+            // 
+            // ucPen
+            // 
+            this.ucPen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ucPen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.ucPen.Imagesurl = global::WiClass.Properties.Resources.pen;
+            this.ucPen.ItemText = "画笔";
+            this.ucPen.Location = new System.Drawing.Point(487, 0);
+            this.ucPen.Name = "ucPen";
+            this.ucPen.Size = new System.Drawing.Size(66, 66);
+            this.ucPen.TabIndex = 1;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1336, 622);
+            this.Controls.Add(this.pnlToolsbar);
+            this.Controls.Add(this.picBackground);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlToolsbar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picStartMenuico)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBackground)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlToolsbar;
+        private System.Windows.Forms.PictureBox picBackground;
+        private System.Windows.Forms.PictureBox picStartMenuico;
+        private UControl.ucMenuItem ucSelect;
+        private UControl.ucMenuItem ucPen;
+        private UControl.ucMenuItem ucEraser;
+        private UControl.ucMenuItem ucTools;
+        private UControl.ucMenuItem ucBackground;
+        private UControl.ucMenuItem ucBack;
+        private UControl.ucMenuItem ucRedo;
+        private UControl.ucMenuItem ucMini;
+        private UControl.ucMenuItem ucMenuItem1;
+    }
+}
---
+++ WiClass/UControl/ucMenuItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WiClass.UControl
+{
+    public partial class ucMenuItem : UserControl
+    {
+        public string ucname;
+        public string ItemText
+        {
+            get {return lblName.Text;}
+            set {lblName.Text = value;}
+        }
+        public Image Imagesurl
+        {
+            get { return picIco.Image; }
+            set { picIco.Image = value; }
+        }
+   
+
+        public ucMenuItem()
+        {
+            InitializeComponent();
+
+        }
+
+ 
+
+
+    }
+}
